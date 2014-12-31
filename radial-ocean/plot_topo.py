2f4ba0da507b39dbdb0ded7a915005bc9939e381
Marsha Berger <berger@cims.nyu.edu>
2014-12-31 13:52:14 -0500

    to plot bathymetry

---
+++ radial-ocean/plot_topo.py
@@ -0,0 +1,32 @@
+"""
+Plot the ocean and island topography from the files 
+    ocean.topotype2
+    island.topotype2
+"""
+
+from pylab import *
+
+from clawpack.geoclaw import topotools
+
+ocean = topotools.Topography()
+ocean.read('ocean.topotype2', 2)
+
+island = topotools.Topography()
+island.read('island.topotype2', 2)
+
+figure(figsize=(8,11))
+
+ax = subplot(2,1,1)
+ocean.plot(axes=ax)
+ax.set_title('ocean topography')
+
+ax = subplot(2,1,2)
+island.plot(axes=ax, contour_levels=[-120,-90,-60,-30,0,30], \
+        contour_kwargs={'colors':'k', 'linestyles':'solid'})
+ax.set_title('zoom on island topography')
+
+show()
+fname = 'topography.png'
+savefig(fname)
+print "Created ",fname
+
